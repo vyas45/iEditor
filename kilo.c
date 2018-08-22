f20a91aaff03216d9d754503f0bfb95edaeca1a5
Aniket Vyas <vyas45@Anikets-MacBook-Pro.local>
2018-08-21 23:59:47 -0700

    Complete Multi-Line read support

--- kilo.c
+++ kilo.c
@@ -266,7 +266,7 @@ void editorOpen(char* filename) {
     char* line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
-    while ((linelen = getline(&line, &linecap, fp) != -1)) {
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
         while (linelen>0 && (line[linelen-1] == '\n' || line[linelen-1] == '\r'))
             linelen--;
         editorAppendRow(line, linelen);
